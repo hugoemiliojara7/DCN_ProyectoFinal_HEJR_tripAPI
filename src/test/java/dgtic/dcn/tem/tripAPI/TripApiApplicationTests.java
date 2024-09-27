38d1a0af0eec04e9fe1de748b5562d9ea8887885
HugoJara <hugoejara7gmail.com>
2024-09-27 18:55:09 +0000

    Estructura inicial del proyecto tripAPI - proyecto final Diplomado CloudNative

---
+++ src/test/java/dgtic/dcn/tem/tripAPI/TripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dgtic.dcn.tem.tripAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
